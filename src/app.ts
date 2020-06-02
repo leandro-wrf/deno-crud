2a3fd4b5ba8981db543009d2351cd5bc09cb6330
leandro-wrf <leandro.ferreira.wr@gmail.com>
2020-06-02 10:06:06 -0300

    add connection database

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
 import router from './routes.ts';
-import './database/dbConnection.ts';
+import './database/dbConfig.ts';
 
 const app = new Application();
 
