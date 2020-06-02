4cac4973d43315f7354cf53f582504cc07ac530b
leandro-wrf <leandro.ferreira.wr@gmail.com>
2020-06-02 10:04:49 -0300

    add model User

---
+++ src/models/User.ts
@@ -0,0 +1,13 @@
+import { DATA_TYPES, Model } from 'https://deno.land/x/denodb/mod.ts';
+
+export const Use = class User extends Model {
+  static table = 'names'
+  static timestamps = true
+  static fields = {
+    id: {
+      primaryKey: true,
+      autoIncrement: true
+    },
+    user: DATA_TYPES.STRING
+  }
+}
